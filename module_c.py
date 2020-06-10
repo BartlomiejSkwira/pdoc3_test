9c1a020396edb34c7e39f50d049f9a20546c1473
Bartek Skwira <bartek.skwira@autologyx.com>
2020-06-10 11:20:33 +0200

    Add module_d with no docstrings

--- module_c.py
+++ module_c.py
@@ -8,6 +8,10 @@ def module_function():
     This is a docstring for module_function
     :return:
     """
+    function_variable = 10
+    """
+    This is a docstring for function_variable
+    """
 
 
 def _private_module_function():
---
+++ module_d.py
@@ -0,0 +1,24 @@
+# FYI: This module and class intentionally has no docstrings.
+
+
+module_variable = 1
+
+
+class NoDocStrings:
+    class_variable = 2
+
+    def __init__(self):
+        self.instance_variable = 3
+
+    def foo(self):
+        pass
+
+    def _private_foo(self):
+        pass
+
+    def __name_mangled_function(self):
+        pass
+
+
+def module_function():
+    pass
